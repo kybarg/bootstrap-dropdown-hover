54374f528be08620e593809d6b179833a7781bef
Ruslan Kyba <kybargr@gmail.com>
2018-11-06 13:39:49 +0200

    Fix events

--- js/bootstrap-dropdownhover.js
+++ js/bootstrap-dropdownhover.js
@@ -72,20 +72,20 @@
     if (e && e.which === 3) return
     $(backdrop).remove()
     $('[data-hover="dropdown"]').each(function () {
-      var $this         = $(this)
-      var $parent       = getParent($this)
+      var $this = $(this)
+      var $parent = getParent($this)
       var relatedTarget = { relatedTarget: this }
 
       if (!$parent.hasClass('open')) return
 
       if (e && e.type == 'click' && /input|textarea/i.test(e.target.tagName) && $.contains($parent[0], e.target)) return
 
-      $parent.trigger(e = $.Event('hide.bs.dropdown', relatedTarget))
+      $parent.trigger(e = $.Event('hide.bs.dropdownhover', relatedTarget))
 
       if (e.isDefaultPrevented()) return
 
       $this.attr('aria-expanded', 'false')
-      $parent.removeClass('open').trigger($.Event('hidden.bs.dropdown', relatedTarget))
+      $parent.removeClass('open').trigger($.Event('hidden.bs.dropdownhover', relatedTarget))
     })
   }
 
